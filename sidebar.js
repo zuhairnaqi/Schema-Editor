1687c74e162d505eb1fe05482f49acfa39459c1c
taimooralqureshi <taimooralqureshi@gmail.com>
2021-10-28 11:16:37 +0500

    fix: save button color change fix: side modal moved to right fix: side modal width set upto 40%

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,7 @@
 /* Set the width of the side navigation to 250px */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "50%";
+function openNav($event) {
+  document.getElementById("mySidenav").style.width = "40%";
+  $event.stopPropagation();
 }
 
 /* Set the width of the side navigation to 0 */
