dbcd94473ffe097eed07c266bac3e0807a433148
taimooralqureshi <taimooralqureshi@gmail.com>
2021-10-28 21:28:48 +0500

    feat(sidebar-modal): add and remove fields from side modal

--- sidebar.js
+++ sidebar.js
@@ -10,3 +10,46 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.querySelector(".overlay").style.display = "none";
 }
+
+
+const fieldCreator = (index) => {
+  const row = document.createElement("div");
+  row.setAttribute("class", "field-row");
+  row.setAttribute("id", `${index}`);
+  row.insertAdjacentHTML(
+    "beforeend",
+    `
+      <input class="sidebarFieldInput" placeholder="Field Name" type="text" />
+      <select class="sidebarFieldInput">
+        <option>Choose Data</option>
+      </select>
+      <label class="letterN">N</label>
+      <i class="fa fa-gear" role="button" id="gear-button"></i>
+      <i
+        onclick="removeField(event)"
+        class="fa fa-trash-o"
+        style="margin-left: 20px; font-size: 20px; color: red"
+      ></i>
+  `
+  );
+  return row;
+};
+
+let = fieldArray = [];
+
+function addfield() {
+  fieldArray.push(fieldCreator(fieldArray.length));
+  updateDom();
+}
+
+function removeField($event) {
+  $event.target.parentNode.parentNode.removeChild($event.target.parentNode);
+  fieldArray = fieldArray.filter((_, i) => i === $event.target.parentNode.id);
+}
+
+function updateDom() {
+  document
+    .querySelector(".field-form-container")
+    .replaceChildren(...fieldArray);
+}
+addfield();
